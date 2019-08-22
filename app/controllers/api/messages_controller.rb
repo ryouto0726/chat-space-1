e3b9d86ca4cd5a2e421d3cb1f22564fa8531901d
masuteru <k.kishimoto50@gmail.com>
2019-08-22 15:32:04 +0900

    api::MessagesControllerを修正
    タイポミス

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,4 @@
-class Api::MessagesController < ApplicationCtontroller
+class Api::MessagesController < ApplicationController
   def index
     group = Group.find(params[:group_id])
     last_message_id = params[:id].to_i
